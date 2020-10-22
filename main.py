16281c1a39a9dd01abe477d9467ac9210eead395
Nyelsonon <51932119+Nyelsonon@users.noreply.github.com>
2020-10-22 14:10:16 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,50 @@
+#imports
+
+import cv2
+
+import pytesseract
+
+import numpy as np
+
+import pyautogui
+
+import time
+
+#variables
+#coordinate variables -DYNAMIC BASED ON COMPUTER- these are complicated, they are the region in which the health picture is taken. This depends on many factors such as fullscreen or not, and your resolution
+x1 = 0
+y1 = 0
+x2 = 5120
+y2 = 2880
+#dont change these:
+true = "true"
+img = "image1.png"
+#you can change these tho:
+update_time = 1
+
+def update_image():
+	time.sleep(update_time)
+	im1 = pyautogui.screenshot(region=(x1,y1,x2,y2))
+	"""this path  down here WILL change  based on the comuter, dont fret Nyelson is here to provide you instructions.
+	 MAC: left click on the folder you want to keep the proccess in, press alt and press copy path
+	 WINDOWS: eh search it up
+	 """
+	im1.save('/Users/nealkotval/Desktop/ocr/image1.png')
+	img = cv2.imread('image1.png')
+    
+#code stuff
+while true == "true":
+	update_image()
+	img = cv2.imread('image1.png')
+	
+	#grayscale stuffs dont mess with this
+	gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+	gray, img_bin = cv2.threshold(gray,128,255,cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+	gray = cv2.bitwise_not(img_bin)
+	kernel = np.ones((2, 1), np.uint8)
+	img = cv2.erode(gray, kernel, iterations=1)
+	img = cv2.dilate(img, kernel, iterations=1)
+
+
+	text = pytesseract.image_to_string(img)
+	print(text)
