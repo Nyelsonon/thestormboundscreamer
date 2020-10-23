a6570c9ad88ff5c9e955ae35230d8ca9284f4d66
Nyelsonon <51932119+Nyelsonon@users.noreply.github.com>
2020-10-23 09:35:01 -0400

    Create coordfinder.py

---
+++ coordfinder.py
@@ -0,0 +1,15 @@
+"""
+instructions, run the script and hover your mouse in a position,
+the script will print outt its coords every second, you can write down the x and y coords and use them for my stormbound screamer script
+"""
+#imports
+import pyautogui
+import time
+
+#code
+while True:
+	print(pyautogui.position())
+	time.sleep(1)
+  
+ #you have to import stuffs for this scrip to but check the read me fil fo rmore info
+ 
